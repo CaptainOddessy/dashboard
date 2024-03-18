41e0a3d0c39bee96d142970ba23b2c01eff4561f
captainoddessy <captainoddessy@gmail.com>
2024-03-18 15:26:09 +0600

    mah

---
+++ app/mah/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Page() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-opacity-25 bg-green-500 backdrop-filter backdrop-blur-lg">
+      <p className="text-3xl text-gray-800 text-center font-bold shadow-lg p-8 rounded-lg bg-green-50 bg-opacity-75">Mahabub</p>
+    </div>
+  );
+}
